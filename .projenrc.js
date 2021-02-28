ea71b68c8e54fbdd0020a9a6e92782f21c1d5436
shazi7804 <shazi7804@gmail.com>
2021-02-28 21:27:51 +0800

    dd

--- .projenrc.js
+++ .projenrc.js
@@ -1,4 +1,55 @@
-const { JsiiProject } = require('projen');
+// const { JsiiProject } = require('projen');
+
+// const project = new JsiiProject({
+//   name: 'cdktf-aws-secure',
+//   authorName: 'Scott Liao',
+//   authorEmail: 'shazi7804@gmail.com',
+//   repository: 'https://github.com/shazi7804/cdktf-aws-secure-constructs.git',
+//   description: 'High level CDKTF construct to provision secure configurations with AWS',
+//   license: 'Apache-2.0',
+//   defaultReleaseBranch: 'main',
+//   autoDetectBin: false,
+//   dependabot: false,
+//   cdkDependencies: [
+//     '@aws-cdk/core',
+//     '@aws-cdk/aws-iam',
+//   ],
+//   python: {
+//     distName: 'cdktf-aws-secure',
+//     module: 'cdktf_aws_secure',
+//   },
+//   deps: [
+//     'cdktf@0.1.0',
+//     '@cdktf/provider-aws@^1.0.7',
+//     'constructs@^3.0.0',
+//     'projen',
+//   ],
+//   devDeps: [
+//     'cdktf@0.1.0',
+//     '@cdktf/provider-aws@^1.0.7',
+//     'constructs@^3.0.0',
+//   ],
+//   peerDeps: [
+//     'cdktf@0.1.0',
+//     '@cdktf/provider-aws@^1.0.7',
+//     'constructs@^3.0.0',
+//     'projen',
+//   ],
+// });
+
+// project.gitignore.exclude('**/*.js');
+// project.gitignore.exclude('**/*.d.ts');
+// project.gitignore.exclude('package-lock.json');
+
+// project.synth();
+
+
+
+const { JsiiProject, Semver } = require('projen');
+
+const cdktfVersion = Semver.caret('0.0.16')
+const constructsVersion = Semver.caret('3.0.0')
+const awsProviderVersion = Semver.caret('1.0.7')
 
 const project = new JsiiProject({
   name: 'cdktf-aws-secure',
@@ -8,37 +59,23 @@ const project = new JsiiProject({
   description: 'High level CDKTF construct to provision secure configurations with AWS',
   license: 'Apache-2.0',
   defaultReleaseBranch: 'main',
-  autoDetectBin: false,
-  dependabot: false,
-  cdkDependencies: [
-    '@aws-cdk/core',
-    '@aws-cdk/aws-iam',
-  ],
-  python: {
-    distName: 'cdktf-aws-secure',
-    module: 'cdktf_aws_secure',
+  dependencies: {
+    '@cdktf/provider-github': awsProviderVersion
   },
-  deps: [
-    'cdktf@0.1.0',
-    '@cdktf/provider-aws@^1.0.7',
-    'constructs@^3.0.0',
-    'projen',
-  ],
-  devDeps: [
-    'cdktf@0.1.0',
-    '@cdktf/provider-aws@^1.0.7',
-    'constructs@^3.0.0',
-  ],
-  peerDeps: [
-    'cdktf@0.1.0',
-    '@cdktf/provider-aws@^1.0.7',
-    'constructs@^3.0.0',
-    'projen',
-  ],
+  devDependencies: {
+    '@cdktf/provider-github': awsProviderVersion,
+    'cdktf': cdktfVersion,
+    'constructs': constructsVersion
+  },
+  peerDependencies: {
+    'cdktf': cdktfVersion,
+    'constructs': constructsVersion,
+    '@cdktf/provider-github': awsProviderVersion
+  }
 });
 
-project.gitignore.exclude('**/*.js');
-project.gitignore.exclude('**/*.d.ts');
-project.gitignore.exclude('package-lock.json');
+project.gitignore.exclude('**/*.js')
+project.gitignore.exclude('**/*.d.ts')
+project.gitignore.exclude('package-lock.json')
 
 project.synth();
